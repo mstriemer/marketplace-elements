5cf819635781ed3aef6d5238020ec80593bf46b5
Mark Striemer <mstriemer@mozilla.com>
2014-12-17 21:40:15 -0600

    Render the demo through JS

---
+++ gh-pages/index.js
@@ -0,0 +1,56 @@
+(function () {
+    var elements = [];
+
+    elements.push({
+        name: "mkt-banner",
+        template: getTemplate("mkt-banner-template"),
+        attributes: {
+            success: {
+                description: "Make it successful.",
+            },
+            dismiss: {
+                options: ["on", "off", "remember"],
+                description: "Handle the dismiss button."
+            },
+        },
+    });
+
+    elements.push({
+        name: "mkt-segmented",
+        template: getTemplate("mkt-segmented-template"),
+        events: {
+            change: "Fired when the selection changes",
+        },
+        properties: {
+            value: "The value of the currently selected option.",
+        },
+    });
+
+    elements.push({
+        name: "mkt-tab-control",
+        template: getTemplate("mkt-tab-control-template"),
+        events: {
+            change: "Fired when the selection changes.",
+        },
+        properties: {
+            value: "The value of the currently selected option.",
+        },
+    });
+
+    function getTemplate(templateId) {
+        return document.getElementById(templateId).innerHTML;
+    }
+
+    function renderTemplate(templateId, context) {
+        return Mustache.render(getTemplate(templateId), context);
+    }
+
+    document.body.innerHTML = [
+        renderTemplate('element-listing', {elements: elements}),
+        "<hr>",
+        renderTemplate('element-detail-tabs', {
+            elements: elements,
+            elementDetail: getTemplate('element-detail'),
+        }),
+    ].join("\n");
+})();
